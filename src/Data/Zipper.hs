54072d6e94856ac8958abf2aa21639201f24eeb5
Trouble-Truffle <perigordtruffle7318@gmail.com>
2022-03-08 14:03:39 +0800

    Data.Zipper

--- src/Data/Zipper.hs
+++ src/Data/Zipper.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, TemplateHaskell, TypeApplications#-}
+{-# LANGUAGE TypeFamilies, TemplateHaskell, TypeApplications, FlexibleInstances, OverloadedLists #-}
 module Data.Zipper where
 
 import qualified Data.Sequence as S
@@ -6,10 +6,12 @@ import Data.Sequence (ViewL(..), ViewR(..), viewl, viewr, (<|), (|>))
 import Control.Lens hiding ((:>), (:<), (|>), (<|))
 import Control.Comonad
 
+import Snife.Util
+
 import Control.Monad
 import Data.Foldable
+import Data.List
 
-import Snife.Util
 
 
 class Zipper z where
@@ -18,11 +20,14 @@ class Zipper z where
   shift :: Direction z -> z a -> z a
   neighbors :: z a -> [a]
 
+-- Looped Zip <Begin> -----------------------------------
 data LoopedZip a = LoopedZip {
     _focus :: a
   , _conts :: S.Seq a
-  } deriving (Show)
+  }
 makeLenses ''LoopedZip
+instance Show a => Show (LoopedZip a)  where
+  show (LoopedZip foc conts) = ("[" ++ show foc ++ "]") ++ concat ( toList $ S.intersperse "|" $ fmap show conts)
 
 instance Zipper LoopedZip where
 
@@ -48,12 +53,37 @@ instance Functor LoopedZip where
 
 instance Comonad LoopedZip where
   co_return  = _focus
-  co_join zipper = LoopedZip zipper $ 
-                  S.fromFunction (pred $ S.length $ zipper ^. conts) 
+  co_join zipper = LoopedZip zipper $
+                  S.fromFunction (pred $ S.length $ zipper ^. conts)
                     (\k -> succ k `composeN` shift L $ zipper)
+-- Looped Zip <End> -----------------------------------
 
+-- Grid Zip <Begin> -------------------------------------
+newtype GridZip z a = GridZip { _unZip :: z (z a) }
+makeLenses ''GridZip
 
+type GZ = GridZip LoopedZip
 
+instance Show a => Show (GZ a) where
+  show (GridZip lz) = (\(LoopedZip foc conts) -> unlines $ ("<" ++ foc ++ ">") : toList conts)
+    $ fmap show lz
+
+instance Zipper GZ where
+  data Direction GZ = N | E | S | W
+  shift E = (& unZip %~ shift L)
+  shift W = (& unZip %~ shift R)
+  shift S = (& unZip %~ fmap (shift L))
+  shift N = (& unZip %~ fmap (shift R))
+  neighbors (GridZip zipper) = neighbors (zipper^.focus) ++ concatMap neighbors' (neighbors zipper)
+    where
+    neighbors' z = (z^.focus) : neighbors z
+
+matrixToGZip :: [[a]] -> GZ a
+matrixToGZip = GridZip . g . map g
+  where 
+    g [] = error "Empty matrix"
+    g (x:xs) = LoopedZip x $ S.fromList xs
+-- Grid Zip <End> -------------------------------------
 
 
 
